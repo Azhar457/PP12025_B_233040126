e7b92b60ae482a3513396d22be787c722f16ffc5
Azhar457 <azharsss457@gmail.com>
2025-02-12 11:59:30 +0700

    Update

--- Pertemuan1/NodeMain2.java
+++ Pertemuan1/NodeMain2.java
@@ -1,6 +1,6 @@
 package Pertemuan1;
 
-public class NodeMain {
+public class NodeMain2 {
 	public static void main(String[] args) {
 		
 		//Membuat 2 Buah Node n1 & n2
@@ -20,23 +20,29 @@ public class NodeMain {
 		//Membuat Relasi Node n3 & n4 untuk soal 4 A
 		n3.setNext(n4);
 		
+		//
 		n5.setNext(n6);
-
+		//
 		n6.setNext(n1);
+		//
+//		n1.setNext(n2); Tidak usah karena sudah mengikuti node awal
+		//		
+//		n2.setNext(n3); 
 		
 		//Menampilkan Node dengan pointer p dan p1
-//		Node p = n1;
+		Node p = n1;
 		Node p1 = n5;
-//		while(p != null)
-//		{
-//			System.out.printf("%d ", p.getNilai());
-//			p = p.getNext();
-//		}
-//		System.out.println("");
+		while(p != null)
+		{
+			System.out.printf("%d ", p.getNilai());
+			p = p.getNext();
+		}
+		System.out.println("");
 		while(p1 != n4)
 		{
 			System.out.printf("%d ", p1.getNilai());
 			p1 = p1.getNext();
 		}
 	}
+	
 }
